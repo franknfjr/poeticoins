46be9c66a683f984ee6bdab107f37145781c4efa
franknfjr <frankdonascimentojr@gmail.com>
2023-04-21 19:43:31 -0300

    chore: live components

--- lib/poeticoins_web/live/crypto_dashboard_live.ex
+++ lib/poeticoins_web/live/crypto_dashboard_live.ex
@@ -4,7 +4,7 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
   import PoeticoinsWeb.ProductHelpers
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, trades: %{}, products: [], filter_products: & &1)
+    socket = assign(socket, products: [])
     {:ok, socket}
   end
 
@@ -31,53 +31,19 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
       </form>
     </div>
 
-    <%= for product <- @products, trade = @trades[product] do%>
-
-    <div class="product-component">
-    <div class="currency-container">
-      <img class="icon" src="<%= crypto_icon(@socket, product) %>" />
-      <div class="crypto-name">
-        <%= crypto_name(product) %>
-      </div>
-    </div>
-
-    <div class="price-container">
-      <ul class="fiat-symbols">
-        <%= for fiat <- fiat_symbols() do %>
-          <li class="
-          <%= if fiat_symbol(product) == fiat, do: "active" %>
-            "><%= fiat %></li>
-        <% end %>
-      </ul>
-
-      <div class="price">
-        <%= trade.price %>
-        <%= fiat_character(product) %>
-      </div>
-    </div>
-
-    <div class="exchange-name">
-      <%= product.exchange_name %>
-    </div>
-
-    <div class="trade-time">
-      <%= human_datetime(trade.traded_at) %>
-    </div>
-    </div>
-
+    <%= for product <- @products do%>
+      <%= live_component @socket, PoeticoinsWeb.ProductComponent, id: product %>
     <% end %>
     """
   end
 
   def handle_info({:new_trade, trade}, socket) do
-    socket = update(socket, :trades, &Map.put(&1, trade.product, trade))
-
+    send_update(PoeticoinsWeb.ProductComponent, id: trade.product, trade: trade)
     {:noreply, socket}
   end
 
   def handle_event("add-product", %{"product_id" => product_id} = _params, socket) do
-    [exchange_name, currency_pair] = String.split(product_id, ":")
-    product = Product.new(exchange_name, currency_pair)
+    product = product_from_string(product_id)
     socket = maybe_add_product(socket, product)
     {:noreply, socket}
   end
@@ -96,33 +62,36 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
     {:noreply, socket}
   end
 
+  def handle_event("remove-product", %{"product-id" => product_id} = _params, socket) do
+    product = product_from_string(product_id)
+    Poeticoins.unsubcribe_from_trades(product)
+
+    socket = update(socket, :products, &List.delete(&1, product))
+    {:noreply, socket}
+  end
+
   def add_product(socket, product) do
     Poeticoins.subscribe_to_trades(product)
 
-    socket
-    |> update(:products, &(&1 ++ [product]))
-    |> update(:trades, fn trades ->
-      trade = Poeticoins.get_last_trade(product)
-      Map.put(trades, product, trade)
-    end)
+    update(socket, :products, &(&1 ++ [product]))
   end
 
   @spec maybe_add_product(Phoenix.LiveView.Socket.t(), Product.t()) :: Phoenix.LiveView.Socket.t()
   defp maybe_add_product(socket, product) do
     if product not in socket.assigns.products do
-      socket
-      |> add_product(product)
-      |> put_flash(
-        :info,
-        "#{product.exchange_name} - #{product.currency_pair} added successfully"
-      )
+      add_product(socket, product)
     else
-      put_flash(socket, :error, "The product was already added")
+      socket
     end
   end
 
   defp grouped_products_by_exchange_name do
     Poeticoins.available_products()
     |> Enum.group_by(& &1.exchange_name)
+  end
+
+  defp product_from_string(product_id) do
+    [exchange_name, currency_pair] = String.split(product_id, ":")
+    Product.new(exchange_name, currency_pair)
   end
 end
